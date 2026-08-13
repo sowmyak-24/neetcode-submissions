6ecb31fa48e3a3522c7c353c11d2996a55a64fcc
sowmyak-24 <sy2pktwtbm@privaterelay.appleid.com>
2026-08-13 10:26:29 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 4.8
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = [4,5]
+print(type(variable))
